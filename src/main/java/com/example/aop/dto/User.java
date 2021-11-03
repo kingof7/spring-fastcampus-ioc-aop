16ed41bff660c68a58d2c1c966fcb74295cd9fe6
jhlee <vlfl0@naver.com>
2021-11-03 17:46:56 +0900

    initial commit

---
+++ src/main/java/com/example/aop/dto/User.java
@@ -0,0 +1,15 @@
+package com.example.aop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String id;
+    private String pw;
+    private String email;
+}
